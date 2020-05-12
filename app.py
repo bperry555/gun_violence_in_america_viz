99ff5eca05d9d17d1615b2889f882e4cf8f31e3a
Brian Perry <blperry5@gmail.com>
2020-05-11 17:47:13 -0700

    switching branches

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import flask_marshmallow
+
 app = Flask(__name__)
+
+DATABASE = 'assets/data/clean_gunvio_vol4.sqlite' 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'assets/data/clean_gunvio_vol4.sqlite'
+
+db = SQLAlchemy(app)
+
+
 
 
 @app.route('/')
@@ -8,3 +19,7 @@ def index():
 
 if __name__== "__main__":
     app.run(debug=True)
+
+
+
+ 
